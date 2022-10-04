4895e1de8851cf8235fa6b9b898568f51bbc9497
Cory Leach <cory@pracystudios.com>
2022-10-04 10:31:43 -0500

    AssetCache implemented using KeyedAssetCache

--- Runtime/AssetCache.cs
+++ Runtime/AssetCache.cs
@@ -9,191 +9,13 @@ namespace Gameframe.AssetCache
     /// Maintains a cache of assets
     /// </summary>
     /// <typeparam name="TAssetType">The kind of asset to be cached</typeparam>
-    public class AssetCache<TAssetType> where TAssetType : class
+    public class AssetCache<TAssetType> : KeyedAssetCache<string,TAssetType> where TAssetType : class
     {
-        /// <summary>
-        /// Reference to a cached asset.
-        /// Dispose() should be called to release the asset reference.
-        /// </summary>
-        public interface ICachedAsset : IDisposable
-        {
-            ICachedAsset Clone();
-            TAssetType Asset { get; }
-            int RefCount { get; }
-        }
-
-        protected class AssetCacheEntry : IDisposable
-        {
-            private readonly string _assetPath;
-            public string AssetPath => _assetPath;
-
-            private int _refCount = 0;
-            public int RefCount => _refCount;
-
-            public TAssetType Asset => _task?.Result;
-
-            private Task<TAssetType> _task;
-            public Task<TAssetType> Task => _task;
-
-            private readonly Action<TAssetType> _assetDestroyer;
-
-            private bool _disposed = false;
-
-            public AssetCacheEntry(string assetPath, Task<TAssetType> getAssetTask, Action<TAssetType> assetDestroyer)
-            {
-                _assetPath = assetPath;
-                _assetDestroyer = assetDestroyer;
-                _task = getAssetTask;
-            }
-
-            public CachedAsset GetCachedAsset()
-            {
-                if (_disposed)
-                {
-                    throw new InvalidOperationException("AssetCacheEntry cannot get asset after dispose.");
-                }
-                return new CachedAsset(this);
-            }
-
-            public void Increment()
-            {
-                if (_disposed)
-                {
-                    throw new InvalidOperationException("AssetCacheEntry cannot increment after dispose.");
-                }
-                _refCount++;
-            }
-
-            public void Decrement()
-            {
-                if (_disposed)
-                {
-                    throw new InvalidOperationException("AssetCacheEntry cannot decrement after dispose.");
-                }
-                _refCount--;
-            }
-
-            public void Dispose()
-            {
-                if (_disposed)
-                {
-                    throw new InvalidOperationException("AssetCacheEntry already disposed");
-                }
-                _disposed = true;
-                if (!_task.IsCompleted)
-                {
-                    _task.ContinueWith((assetTask) => { _assetDestroyer(assetTask.Result); }, TaskContinuationOptions.OnlyOnRanToCompletion);
-                }
-                else
-                {
-                    _assetDestroyer(_task.Result);
-                }
-                _task = null;
-            }
-        }
-
-        protected class CachedAsset : ICachedAsset
-        {
-            private readonly AssetCacheEntry _cacheEntry;
-            private bool _disposed;
-
-            public CachedAsset(AssetCacheEntry entry)
-            {
-                _disposed = false;
-                _cacheEntry = entry;
-                _cacheEntry.Increment();
-            }
-
-            public ICachedAsset Clone()
-            {
-                return new CachedAsset(_cacheEntry);
-            }
-
-            public TAssetType Asset
-            {
-                get
-                {
-                    if (_disposed)
-                    {
-                        throw new InvalidOperationException("CachedAsset has been disposed");
-                    }
-                    return _cacheEntry.Asset;
-                }
-            }
-
-            public int RefCount => _cacheEntry.RefCount;
-
-            public void Dispose()
-            {
-                if (_disposed)
-                {
-                    throw new InvalidOperationException("CachedAsset has already been disposed");
-                }
-                _disposed = true;
-                _cacheEntry.Decrement();
-            }
-        }
-
-        protected readonly Dictionary<string, AssetCacheEntry> CacheDictionary = new Dictionary<string, AssetCacheEntry>();
-
-        private readonly IAssetLoader<TAssetType> _assetLoader;
-
-        private async Task<TAssetType> LocateAssetAsync(string assetPath)
-        {
-            var asset = await _assetLoader.LoadAsync(assetPath);
-            return asset;
-        }
-
-        /// <summary>
-        /// AssetCache Constructor
-        /// </summary>
-        /// <param name="loader">loads and unloads assets</param>
-        public AssetCache(IAssetLoader<TAssetType> loader)
-        {
-            _assetLoader = loader;
-        }
-
-        /// <summary>
-        /// Get Cached Asset
-        /// </summary>
-        /// <param name="assetPath">Path to asset. Should be handle-able by the asset locator</param>
-        /// <returns>Task that returns the cached asset</returns>
-        public async Task<ICachedAsset> GetAsync(string assetPath)
-        {
-            //Get or create cache entry
-            if (!CacheDictionary.TryGetValue(assetPath, out var entry))
-            {
-                entry = new AssetCacheEntry(assetPath, LocateAssetAsync(assetPath), _assetLoader.Unload);
-                CacheDictionary.Add(assetPath, entry);
-            }
-
-            //Wait for the task to finish
-            if (!entry.Task.IsCompleted)
-            {
-                await entry.Task;
-            }
-
-            return entry.GetCachedAsset();
-        }
-
-        /// <summary>
-        /// Called to clean up and unload assets that have no remaining references
-        /// </summary>
-        public void ClearUnusedAssets()
-        {
-            //Remove Unused entries
-            var unusedEntries = CacheDictionary.Where((pair) => (pair.Value.RefCount <= 0)).ToList();
-            foreach (var pair in unusedEntries)
-            {
-                CacheDictionary.Remove(pair.Key);
-                pair.Value.Dispose();
-            }
-        }
-
+        public AssetCache(IKeyedAssetLoader<string, TAssetType> loader) : base(loader) { }
     }
 
     /// <summary>
-    /// Maintains a cache of assets
+    /// Maintains a cache of assets with a specified key type and asset type
     /// </summary>
     /// <typeparam name="TAssetType">The kind of asset to be cached</typeparam>
     /// <typeparam name="TKeyType">The key used to reference the asset to be loaded/unloaded</typeparam>
@@ -344,15 +166,15 @@ namespace Gameframe.AssetCache
         /// <summary>
         /// Get Cached Asset
         /// </summary>
-        /// <param name="assetPath">Path to asset. Should be handle-able by the asset locator</param>
+        /// <param name="assetKey">Key for the given asset. Should be handle-able by the asset locator</param>
         /// <returns>Task that returns the cached asset</returns>
-        public async Task<ICachedAsset> GetAsync(TKeyType assetPath)
+        public async Task<ICachedAsset> GetAsync(TKeyType assetKey)
         {
             //Get or create cache entry
-            if (!CacheDictionary.TryGetValue(assetPath, out var entry))
+            if (!CacheDictionary.TryGetValue(assetKey, out var entry))
             {
-                entry = new AssetCacheEntry(assetPath, LocateAssetAsync(assetPath), _assetLoader.Unload);
-                CacheDictionary.Add(assetPath, entry);
+                entry = new AssetCacheEntry(assetKey, LocateAssetAsync(assetKey), _assetLoader.Unload);
+                CacheDictionary.Add(assetKey, entry);
             }
 
             //Wait for the task to finish
--- Runtime/IAssetLoader.cs
+++ Runtime/IAssetLoader.cs
@@ -6,20 +6,8 @@ namespace Gameframe.AssetCache
     /// Interface for loading and unloading assets in AssetCache
     /// </summary>
     /// <typeparam name="TAssetType">UnityEngine.Object asset type</typeparam>
-    public interface IAssetLoader<TAssetType> where TAssetType : class
+    public interface IAssetLoader<TAssetType> : IKeyedAssetLoader<string,TAssetType> where TAssetType : class
     {
-        /// <summary>
-        /// Loads an asset asynchronously
-        /// </summary>
-        /// <param name="assetPath">path to the asset to be loaded</param>
-        /// <returns>Task that returns the asset type</returns>
-        Task<TAssetType> LoadAsync(string assetPath);
-
-        /// <summary>
-        /// Unloads an asset
-        /// </summary>
-        /// <param name="asset">Asset to be unloaded</param>
-        void Unload(TAssetType asset);
     }
 
     /// <summary>
